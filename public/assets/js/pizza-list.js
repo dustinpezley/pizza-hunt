9f81bf17468f4429428935fe7d4b647e7a221a37
dustinpezley <dustinpezley@gmail.com>
2022-08-29 20:09:10 -0500

    comment functionality on back-end built

--- public/assets/js/pizza-list.js
+++ public/assets/js/pizza-list.js
@@ -1,6 +1,22 @@
 const $pizzaList = document.querySelector('#pizza-list');
+const getPizzaList = () => {
+  fetch('/api/pizzas')
+    .then((response) => response.json())
+    .then((pizzaListArr) => {
+      pizzaListArr.forEach(printPizza);
+    })
+    .catch((err) => console.error(err));
+};
 
-const printPizza = ({ _id, pizzaName, toppings, size, commentCount, createdBy, createdAt }) => {
+const printPizza = ({
+  _id,
+  pizzaName,
+  toppings,
+  size,
+  commentCount,
+  createdBy,
+  createdAt,
+}) => {
   const pizzaCard = `
     <div class="col-12 col-lg-6 flex-row">
       <div class="card w-100 flex-column">
@@ -13,7 +29,7 @@ const printPizza = ({ _id, pizzaName, toppings, size, commentCount, createdBy, c
           <h5 class="text-dark">Toppings</h5>
           <ul>
             ${toppings
-              .map(topping => {
+              .map((topping) => {
                 return `<li>${topping}</li>`;
               })
               .join('')}
@@ -26,3 +42,5 @@ const printPizza = ({ _id, pizzaName, toppings, size, commentCount, createdBy, c
 
   $pizzaList.innerHTML += pizzaCard;
 };
+
+getPizzaList();
